806257eadcfc51acb47e88a72e0c29732e9dcb41
Brandon Vo <b4vo@uwaterloo.ca>
2025-01-03 00:46:39 -0500

    3.2.3: Fix oval shaped avatars on mobile, clean up

--- src/constants/verifiedUserIDs.ts
+++ src/constants/verifiedUserIDs.ts
@@ -1,6 +1,8 @@
-export const verifiedUserIDs: string[] = [
+const verifiedUserIDs: string[] = [
   "3y8XlQiLCscOR3uQ7UwrSCHgF932",
   "GJTtKYbJG8M2ag3rJdVSpHCGWMz2",
   "5n86oOReOGQX4M8b1Lb3jivjKRT2",
   "S8FGjp8ZmmRTnGbW87aN3S3RaVT2",
 ];
+
+export default verifiedUserIDs;
